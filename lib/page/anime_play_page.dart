e4d980655a39b0855855ca9509c9c6e0f574fd61
ken <c.uvsuv@163.com>
2022-11-14 19:58:05 +0800

    update

--- lib/page/anime_play_page.dart
+++ lib/page/anime_play_page.dart
@@ -1,4 +1,4 @@
-import 'package:chewie/chewie.dart';
+import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_sakura_anime/util/api.dart';
 import 'package:flutter_sakura_anime/util/base_export.dart';
@@ -16,10 +16,19 @@ class AnimePlayPage extends ConsumerStatefulWidget {
 
 class _AnimePlayState extends ConsumerState<AnimePlayPage> {
   late AutoDisposeFutureProvider<String> _playNowUrlProvider;
-  final AutoDisposeStateProvider<ChewieController?> _initProvider =
-      StateProvider.autoDispose<ChewieController?>((ref) => null);
+  final AutoDisposeStateProvider<FlickManager?> _initProvider =
+      StateProvider.autoDispose<FlickManager?>((ref) => null);
   VideoPlayerController? _controller;
-  ChewieController? _chewieController;
+  FlickManager? flickManager;
+  var _totalDuration = 0;
+  final _slideX = StateProvider.autoDispose<Duration>(
+      (ref) => const Duration(milliseconds: 0));
+  final _isShowSlideDialog = StateProvider.autoDispose<bool>((ref) => false);
+  var _slideValue = 0.0;
+  var _downPosition = 0;
+  var _downVolume = 0.0;
+  var _volumeValue = 0.0;
+  VideoPlayerValue? _videoPlayerValue;
 
   @override
   void initState() {
@@ -39,8 +48,7 @@ class _AnimePlayState extends ConsumerState<AnimePlayPage> {
   void dispose() {
     // TODO: implement dispose
     super.dispose();
-    _controller?.dispose();
-    _chewieController?.dispose();
+    flickManager?.dispose();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
         overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
     SystemChrome.setPreferredOrientations(
@@ -54,14 +62,16 @@ class _AnimePlayState extends ConsumerState<AnimePlayPage> {
     var controller = VideoPlayerController.network(playerUrl);
     _controller = controller;
     await controller.initialize();
-    var chewieController = ChewieController(
-      aspectRatio: controller.value.aspectRatio,
-      videoPlayerController: controller,
-      autoPlay: true,
-      looping: true,
-    );
-    _chewieController = chewieController;
-    ref.read(_initProvider.state).state = chewieController;
+
+    flickManager = FlickManager(videoPlayerController: controller);
+    await flickManager?.flickControlManager?.play();
+    flickManager?.flickDisplayManager?.hidePlayerControls();
+
+    _videoPlayerValue = flickManager?.flickVideoManager?.videoPlayerValue;
+
+    _totalDuration = _videoPlayerValue?.duration.inMilliseconds ?? 0;
+
+    ref.read(_initProvider.state).state = flickManager;
   }
 
   @override
@@ -69,6 +79,8 @@ class _AnimePlayState extends ConsumerState<AnimePlayPage> {
     return Consumer(builder: (context, ref, _) {
       var watch = ref.watch(_playNowUrlProvider);
       var controller = ref.watch(_initProvider);
+      var media = MediaQuery.of(context);
+      var sizeHeight = (media.size.width - media.padding.top) / 2.0;
       if (watch.isRefreshing || controller == null) {
         return Container(
           color: Colors.black,
@@ -83,11 +95,108 @@ class _AnimePlayState extends ConsumerState<AnimePlayPage> {
       } else {
         return Material(
           color: Colors.transparent,
-          child: Chewie(
-            controller: controller,
+          child: Stack(
+            children: [
+              Positioned(
+                  left: 55.0,
+                  top: 0,
+                  bottom: 0,
+                  right: 55,
+                  child: FlickVideoPlayer(
+                    flickManager: controller,
+                    systemUIOverlayFullscreen: const [],
+                    systemUIOverlay: const [],
+                    preferredDeviceOrientation: const [
+                      DeviceOrientation.landscapeLeft
+                    ],
+                  )),
+              Positioned(
+                  left: 55,
+                  right: sizeHeight + 55,
+                  top: 125,
+                  bottom: 125,
+                  child: GestureDetector(
+                    onPanDown: (detail){
+                      _downVolume = controller.flickVideoManager?.videoPlayerValue?.volume?? 0.5;
+                    },
+                    onPanUpdate: (details){
+                      // _volumeValue +=
+                      // controller.flickControlManager.setVolume(volume)
+                    },
+                    behavior: HitTestBehavior.opaque,
+                    child: Container(color:Colors.white,),
+                  )),
+              Positioned(
+                left: sizeHeight + 55,
+                right: 55,
+                top: 125,
+                bottom: 125,
+                child: GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  onPanDown: (details) {
+                    _slideValue = 0;
+                    _downPosition = controller.flickVideoManager
+                            ?.videoPlayerValue?.position.inMilliseconds ??
+                        0;
+                    ref.read(_slideX.state).update(
+                        (state) => Duration(milliseconds: _downPosition));
+                  },
+                  onPanUpdate: (details) {
+                    // debugPrint("onPanUpdate ${details.delta.dx}");
+                    _slideValue += details.delta.dx;
+                    if (_slideValue.abs() > 12.0) {
+                      ref
+                          .read(_isShowSlideDialog.state)
+                          .update((state) => true);
+                    }
+                    var offset = 60 * 1000 * _slideValue ~/ sizeHeight;
+                    var nextValue = (_downPosition + offset);
+                    if (nextValue >= _totalDuration) {
+                      nextValue = _totalDuration;
+                    } else if (nextValue <= 0) {
+                      nextValue = 0;
+                    }
+                    ref
+                        .read(_slideX.state)
+                        .update((state) => Duration(milliseconds: nextValue));
+                  },
+                  onPanEnd: (details) {
+                    ref.read(_isShowSlideDialog.state).update((state) => false);
+                    controller.flickControlManager?.seekTo(ref.watch(_slideX));
+                  },
+                  child: Container(),
+                ),
+              ),
+              IgnorePointer(
+                ignoring: true,
+                child: Align(
+                  child: AnimatedOpacity(
+                    duration: const Duration(milliseconds: 250),
+                    opacity: ref.watch(_isShowSlideDialog) ? 1.0 : 0.0,
+                    child: Text(
+                      "${getTimeInDuration(ref.watch(_slideX))}/${getTimeInDuration(Duration(milliseconds: _totalDuration))}",
+                      style: const TextStyle(fontSize: 25.0, color: Colors.white),
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
         );
       }
     });
+  }
+
+  String getTimeInDuration(Duration duration) {
+    String hours = duration.inHours.toString().padLeft(0, '2');
+    String minutes =
+        duration.inMinutes.remainder(60).toString().padLeft(2, '0');
+    String seconds =
+        duration.inSeconds.remainder(60).toString().padLeft(2, '0');
+    if (hours == "0") {
+      return "$minutes:$seconds";
+    } else {
+      return "$hours:$minutes:$seconds";
+    }
   }
 }
